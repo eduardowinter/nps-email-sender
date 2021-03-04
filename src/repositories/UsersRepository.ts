2e8fb5e611d35312a53275cd17ab7c1030183c21
Eduardo Winter <eduardo.wbess@outlook.commit>
2021-03-04 17:16:53 -0300

    first commit

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository } from "typeorm";
+import { Repository } from "typeorm";
+import { User } from "../models/User";
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {}
+
+export { UsersRepository };
